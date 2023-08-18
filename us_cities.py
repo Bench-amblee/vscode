a1c999c3bbd0d60316bf697f2d1754c9e4a2c48a
Bench-amblee <benjamin.t.chamblee@gmail.com>
2023-08-17 19:47:16 -0500

    update file

--- us_cities.py
+++ us_cities.py
@@ -1,6 +1,10 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import geopandas as gpd
 
 file_path = 'data/uscities.csv'
 df = pd.read_csv(file_path)
 df_sorted = df.sort_values(by='population', ascending=False)
 top_200_cities = df_sorted.head(200)
+
+print('top 200 us cities')
