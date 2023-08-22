128b784bb47c86a2817dcbef9e28f157aad46597
Bench-amblee <benjamin.t.chamblee@gmail.com>
2023-08-21 20:48:49 -0500

    st test

--- us_cities.py
+++ us_cities.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import geopandas as gpd
+import streamlit as st
 
 file_path = 'data/uscities.csv'
 df = pd.read_csv(file_path)
@@ -8,3 +9,5 @@ df_sorted = df.sort_values(by='population', ascending=False)
 top_200_cities = df_sorted.head(200)
 
 print('top 200 us cities')
+
+st.write('test')
