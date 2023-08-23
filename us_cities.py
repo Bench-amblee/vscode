f412af312f654be605aeb174d512b9db07f13c5b
Bench-amblee <benjamin.t.chamblee@gmail.com>
2023-08-22 19:22:49 -0500

    vritual enviornment

--- us_cities.py
+++ us_cities.py
@@ -8,6 +8,8 @@ df = pd.read_csv(file_path)
 df_sorted = df.sort_values(by='population', ascending=False)
 top_200_cities = df_sorted.head(200)
 
-print('top 200 us cities')
+sample = top_200_cities.head(10)
 
-st.write('test')
+st.write('top 10 US cities by Population')
+st.dataframe(sample)
+st.write('insert map here')
