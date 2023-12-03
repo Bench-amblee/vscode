03ceba35ecbcf9324f89e2c4a77b6de8be35785b
Bench-amblee <benjamin.t.chamblee@gmail.com>
2023-12-03 15:06:36 -0500

    file structure

--- 2023 - Advent of Code/AdventOfCode_02.py
+++ 2023 - Advent of Code/AdventOfCode_02.py
@@ -26,21 +26,86 @@ In the example above, games 1, 2, and 5 would have been possible if the bag had 
 Determine which games would have been possible if the bag had been loaded with only 12 red cubes, 13 green cubes, and 14 blue cubes. What is the sum of the IDs of those games?
 '''
 
+bag = {'Red': 12, 'Green': 13, "Blue": 14}
+
 G1 = 'Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green'
 G2 = 'Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue'
-G3 = 'Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red'
+G3 = 'Game 3: 8 green, 6 blue, 20 red; 5 blue, 13 green; 5 green'
 G4 = 'Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red'
 G5 = 'Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green'
 
 
 def data_clean(text):
+    data_dict = {}
+    results_dict = {}
+    round_list = []
+
+    # seperate Game header from item data
     clean1 = text.strip().split(":")
 
-    clean2 = clean1[1].strip().split(";")
+    # seperate games into list of games
+    clean2 = clean1[1].strip().split("; ")
 
-    text_dict = {clean1[0]:clean2}
+    # turn item list data into dictionary to match bag format
+    for i in range(len(clean2)):
+        round = clean2[i].split(', ')
 
-    return text_dict
+        for colors in round:
+            quantity, color = colors.split(' ')
+            quantity = int(quantity)  
+            results_dict[color] = quantity
+
+        round_list.append(results_dict)
+        
+    data_dict[clean1[0]] = round_list
+    return data_dict
+
+def valid_bag_game(game):
+    game_clean = data_clean(game)
+    game_name = list(game_clean.keys())[0]
+    round_count = len(list(game_clean.values())[0])
+
+    global red_count
+    global green_count
+    global blue_count
+
+    red_count = 0
+    green_count = 0
+    blue_count = 0
+
+    for i in range(round_count):
+
+        red_round = game_clean[game_name][i-1]['red']
+        if red_round > red_count:
+            red_count = red_round
+
+        green_round = game_clean[game_name][i-1]['green']
+        if green_round > green_count:
+            green_count = green_round
+
+        blue_round = game_clean[game_name][i-1]['blue']
+        if blue_round > blue_count:
+            blue_count = blue_round
+
+    if red_count > bag['Red']:
+        text = game_name + ' is NOT a valid game'
+
+    elif green_count > bag['Green']:
+        text = game_name + ' is NOT a valid game'
+
+    elif blue_count > bag['Blue']:
+        text = game_name + ' is NOT a valid game'
+
+    elif (red_count+green_count+blue_count) > (bag['Red']+bag['Green']+bag['Blue']):
+        text = game_name + ' is NOT a valid game'
+
+    else:
+        text = game_name + ' is a valid game!'
 
 
-print(data_clean(G1))
+    return text
+
+list_of_games = [G1,G2,G3,G4,G5]
+
+for game in list_of_games:
+    print(valid_bag_game(game))
